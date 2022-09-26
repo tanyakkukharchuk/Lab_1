8b7386b224744f9314fdd57d6b179603d4720527
tanyakkuharchuk <tetiana.kukharchuk.pm.2020@lpnu.ua>
2022-09-26 22:23:23 +0300

    Calculator

---
+++ Calculator/CalcClassBrTests/CalcClassTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalcClassBr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcClassBr.Tests
+{
+    [TestClass()]
+    public class CalcClassTests
+    {
+        public TestContext TestContext { get; set; }
+        [DataSource("System.Data.SqlClient" , @"Data Source=DESKTOP-LGA1DK7\SQLEXPRESS;Initial Catalog=testdata;Integrated Security=True", "TestData", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void IABSTest()
+        {
+            //ARRANGE
+            long a = (long)TestContext.DataRow["a"];
+            long expected = (long)TestContext.DataRow["expected"];
+            //ACT
+            var result = CalcClass.IABS(a);
+            //ASSERT
+            Assert.AreEqual(expected, result);
+
+        }
+    }
+}
---
+++ Calculator/CalcClassBrTests1/CalcClassTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalcClassBr;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcClassBr.Tests
+{
+    [TestClass()]
+    public class CalcClassTests
+    {
+        public TestContext TestContext { get; set; }
+        [DataSource("System.Data.SqlClient", @"Data Source=DESKTOP-LGA1DK7\SQLEXPRESS;Initial Catalog=testdata;Integrated Security=True", "unittestdata", DataAccessMethod.Sequential)]
+        [TestMethod]
+        public void IABSTest()
+        {
+            //ARRANGE
+            long a = (long)TestContext.DataRow["a"];
+            string expected = (string)TestContext.DataRow["expected"];
+            long result;
+            //ACT
+            try
+            {
+                result = CalcClass.IABS(a);
+                Assert.AreEqual(Convert.ToInt64(expected), result);
+            }
+            //ASSERT
+            catch (ArgumentOutOfRangeException es)
+            {
+                Assert.AreEqual(expected, "Error");
+            }
+
+        }
+    }
+}
